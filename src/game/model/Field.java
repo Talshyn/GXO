a9d3cd171d7c9d17996305f0879129e5178d37d6
talshyn Satmetova <talshyn_94_39@mail.ru>
2016-02-20 18:44:58 +0600

    Field test Point cordinate

--- src/game/model/Field.java
+++ src/game/model/Field.java
@@ -1,5 +1,8 @@
 package game.model;
 
+import game.model.exception.AlreadyOccupiedException;
+import game.model.exception.InvalidePointException;
+
 import java.awt.*;
 
 /**
@@ -8,16 +11,36 @@ import java.awt.*;
 public class Field {
 
     private static final int FIELD_SIZE = 3;
+    private static final int MIN_CORDINATE = 0;
+    private static final int MAX_CORDINATE = FIELD_SIZE;
     private Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
 
     public int getSize(){
         return FIELD_SIZE;
     }
 
-    public Figure getFigure(final Point point){
+    public Figure getFigure(final Point point) throws InvalidePointException{
+        if(!checkPoint(point)) {
+            throw new InvalidePointException();
+        }
         return field[point.x][point.y];
     }
-    public void setFigure(final Point point, Figure figure){
+    public void setFigure(final Point point, Figure figure) throws InvalidePointException, AlreadyOccupiedException{
+        if(!checkPoint(point)) {
+            throw new InvalidePointException();
+        }
+        /*If the space is not empty*/
+        if(field[point.x][point.y]!=null){
+            throw new AlreadyOccupiedException();
+        }
         field[point.x][point.y] = figure;
+    }
+
+    private boolean checkPoint(Point point){
+    return checkCordinate(point.x) && checkCordinate(point.y);
+    }
+
+    private boolean checkCordinate(final int cordinate){
+        return cordinate >= MIN_CORDINATE && cordinate < MAX_CORDINATE;
     }
 }
