9668ce15fa193e4b6d49c86dd3c19537fe934102
talshyn Satmetova <talshyn_94_39@mail.ru>
2016-02-22 20:56:16 +0600

    Add test by Diagonal

--- tst/controller/WinnerControllerTest.java
+++ tst/controller/WinnerControllerTest.java
@@ -27,7 +27,7 @@ public class WinnerControllerTest {
 
     }
     @Test
-    public void testGetWinnerWhenNoWinner() throws Exception {
+    public void testGetWinnerWhenNoRowWinner() throws Exception {
 
         final WinnerController winnerController = new WinnerController();
         for(int i = 0; i < 3; i++){
@@ -40,6 +40,97 @@ public class WinnerControllerTest {
         }
 
     }
+    @Test
+    public void testGetWinnerWhenWinnerColumn() throws Exception {
 
+        final WinnerController winnerController = new WinnerController();
+        for(int i = 0; i < 3; i++){
+            final Field field = new Field(3);
+            field.setFigure(new Point(0, i), Figure.O);
+            field.setFigure(new Point(1, i), Figure.O);
+            field.setFigure(new Point(2, i), Figure.O);
+            winnerController.getWinner(field);
+            assertEquals(Figure.O,winnerController.getWinner(field));
+
+        }
+
+    }
+    @Test
+    public void testGetWinnerWhenNoWinnerColumn() throws Exception {
+
+        final WinnerController winnerController = new WinnerController();
+        for(int i = 0; i < 3; i++){
+            final Field field = new Field(3);
+            field.setFigure(new Point(0, i), Figure.O);
+            field.setFigure(new Point(1, i), Figure.O);
+            field.setFigure(new Point(2, i), Figure.X);
+            winnerController.getWinner(field);
+            assertNull(winnerController.getWinner(field));
+
+        }
+
+    }
+
+
+    @Test
+    public void testGetWinnerDiagonalByX() throws Exception {
+
+        final WinnerController winnerController = new WinnerController();
+
+            final Field field = new Field(3);
+            field.setFigure(new Point(0, 0), Figure.X);
+            field.setFigure(new Point(1, 1), Figure.X);
+            field.setFigure(new Point(2, 2), Figure.X);
+
+            assertEquals(Figure.X, winnerController.getWinner(field));
+
+
+
+    }
+
+    @Test
+    public void testGetWinnerDiagonalByY() throws Exception {
+
+        final WinnerController winnerController = new WinnerController();
+        final Field field = new Field(3);
+        field.setFigure(new Point(0, 2), Figure.X);
+        field.setFigure(new Point(1, 1), Figure.X);
+        field.setFigure(new Point(2, 0), Figure.X);
+
+            assertEquals(Figure.X, winnerController.getWinner(field));
+
+
+
+    }
+
+    @Test
+    public void testGetWinnerNoDiagonalByY() throws Exception {
+
+        final WinnerController winnerController = new WinnerController();
+        final Field field = new Field(3);
+        field.setFigure(new Point(0, 2), Figure.X);
+        field.setFigure(new Point(1, 1), Figure.X);
+        field.setFigure(new Point(2, 0), Figure.O);
+
+        assertNull( winnerController.getWinner(field));
+
+
+
+    }
+
+    @Test
+    public void testGetWinnerNoDiagonalByX() throws Exception {
+
+        final WinnerController winnerController = new WinnerController();
+        final Field field = new Field(3);
+        field.setFigure(new Point(0, 0), Figure.X);
+        field.setFigure(new Point(1, 1), Figure.X);
+        field.setFigure(new Point(2, 2), Figure.O);
+
+        assertNull(winnerController.getWinner(field));
+
+
+
+    }
 
 }
