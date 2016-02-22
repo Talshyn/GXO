ef2056ff44f5baa3378317e4e49f0690015bff3b
talshyn Satmetova <talshyn_94_39@mail.ru>
2016-02-22 11:21:45 +0600

    Move Controller some fixing

--- src/controller/MoveController.java
+++ src/controller/MoveController.java
@@ -3,12 +3,12 @@ package controller;
 import game.model.Field;
 import game.model.Figure;
 import game.model.exception.AlreadyOccupiedException;
-import game.model.exception.InvalidePointException;
+import game.model.exception.InvalidPointException;
 
 import java.awt.*;
 
 public class MoveController {
-    public void applyFigure(Field field, Point point, Figure figure) throws AlreadyOccupiedException, InvalidePointException {
+    public void applyFigure(Field field, Point point, Figure figure) throws AlreadyOccupiedException, InvalidPointException {
          /*If the space is not empty*/
         if(field.getFigure(point)!=null){
             throw new AlreadyOccupiedException();
